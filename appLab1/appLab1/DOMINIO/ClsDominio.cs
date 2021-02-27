552ea1d16eb4dcb968445950bfe739d8a67e2eee
Milton Orellana <go0211032020@unab.edu.sv>
2021-02-26 19:44:14 -0600

    clase domino y sus atributos

---
+++ appLab1/appLab1/DOMINIO/ClsDominio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appLab1.DOMINIO
+{
+    class ClsDominio
+    {
+
+        private String User;
+        private String pass;
+
+    }
+}
